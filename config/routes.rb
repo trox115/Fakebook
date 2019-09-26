e9caeb5cf216de7f3c355564a1fceefdd671207e
trox115 <tofernandes67@gmail.com>
2019-09-26 23:29:25 +0100

    changed routes so that the user must sign in to see other pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+
+    root 'devise/sessions#new'
+  end
+  devise_scope :users do
+  authenticated :users do
+    root 'home#index', as: :authenticated_root
+  end
+
+  unauthenticated do
+    root 'devise/sessions#new', as: :unauthenticated_root
+  end
+end
 end
