4762eaf4ce1cd94b335f7c4c756492c3b7f54eaa
trox115 <tofernandes67@gmail.com>
2019-09-30 17:09:32 +0100

    paginated users index to show only 10 users per page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
   def index
-    @users= User.all_except(current_user).paginate(page: params[:page], per_page: 1)
+    @users= User.all_except(current_user).paginate(page: params[:page], per_page: 10)
   end
 end
