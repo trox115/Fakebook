43f94d0a7960544c5db216c7217b7930e42401c5
trox115 <tofernandes67@gmail.com>
2019-09-30 16:29:17 +0100

    only signed in users can post

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe PostsController, type: :controller do
   describe 'get new post' do
     it 'returns http success' do
       get :new
-      expect(response).to have_http_status(:success)
+      expect(response).not_to have_http_status(:success)
     end
   end
 
   describe 'get index post' do
     it 'returns http success' do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response).not_to have_http_status(:success)
     end
   end
 end
