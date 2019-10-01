263dd53897f6131e21c9abb4409f6347bca43f00
trox115 <tofernandes67@gmail.com>
2019-10-01 22:57:20 +0100

    Created model for friendships, added model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
-# frozen_string_literal: true
+  #frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+#    Include default devise modules. Others available are:
+#    :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
@@ -15,4 +15,29 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 100 }
   scope :all_except, ->(user) { where.not(id: user) }
+  has_many :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+
+  def friends
+    friends_array = friendships.map{|friendship| friendship.friend if friendship.confirmed}
+    friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.confirmed}
+    friends_array.compact
+  end
+  def pending_friends
+    friendships.map{|friendship| friendship.friend if !friendship.confirmed}.compact
+  end
+
+  def friend_requests
+    inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact
+  end
+
+  def confirm_friend(user)
+    friendship = inverse_friendships.find{|friendship| friendship.user == user}
+    friendship.confirmed = true
+    friendship.save
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
 end
