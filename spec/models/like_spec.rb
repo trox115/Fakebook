9fb29aa306f1e7337207f6b20074e230621fb006
trox115 <tofernandes67@gmail.com>
2019-10-01 18:03:01 +0100

    Added tests for likes

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -3,5 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+context 'Creates a like' do
+  it 'with valid information' do
+    expect(FactoryBot.build(:like)).to be_valid
+  end
+end
+context 'should not create a like' do
+   it 'without a user' do
+    expect(FactoryBot.build(:like, user: nil)).not_to be_valid
+  end
+it 'without a referenced post' do
+    expect(FactoryBot.build(:like, post_id: nil)).not_to be_valid
+  end
+end
+
 end
