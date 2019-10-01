c2ec70200abc21b592946b6e4e014b2ad6fdae9c
trox115 <tofernandes67@gmail.com>
2019-10-01 18:32:26 +0100

    Added some more tests

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -7,6 +7,12 @@ context 'Creates a like' do
   it 'with valid information' do
     expect(FactoryBot.build(:like)).to be_valid
   end
+
+  it 'with valid user and post ids' do
+    test_post = FactoryBot.build(:post)
+    test_user= FactoryBot.build(:user)
+    expect(FactoryBot.build(:like, test_post, test_user)).to be_valid
+  end
 end
 context 'should not create a like' do
    it 'without a user' do
@@ -14,6 +20,14 @@ context 'should not create a like' do
   end
 it 'without a referenced post' do
     expect(FactoryBot.build(:like, post_id: nil)).not_to be_valid
+  end
+
+it 'without a referenced post but with valid user' do
+    expect(FactoryBot.build(:like, post_id: nil, user_id:1)).not_to be_valid
+  end
+
+it 'without a valid user but with a valid post' do
+    expect(FactoryBot.build(:like, post_id:1, user_id:nil)).not_to be_valid
   end
 end
 
