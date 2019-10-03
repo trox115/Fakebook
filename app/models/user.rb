2daefba2cde67e347ea1b5b56371cf0125e44afd
trox115 <tofernandes67@gmail.com>
2019-10-03 17:56:31 +0100

    Oushadowing file

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,9 +34,10 @@ class User < ApplicationRecord
   end
 
   def confirm_friend(user)
-    friendship = inverse_friendships.find { |friendship| friendship.user == user }
-    friendship.confirmed = true
-    friendship.save
+    friendship1 = inverse_friendships.find { |friendship| friendship.user == user }
+    friendship1.confirmed = true
+    friendship1.save
+
   end
 
   def friend?(user)
