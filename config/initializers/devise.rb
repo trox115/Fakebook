032cf46d6ab60f73566ce3b516f52f9c71ce7658
Carlos Del Real <carloshdelreal@gmail.com>
2019-10-04 17:51:53 -0500

    add omniath-facebook, its working

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -296,4 +296,5 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+  config.omniauth :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_KEY'], callback_url: "http://localhost:3000/users/auth/facebook/callback"
 end
