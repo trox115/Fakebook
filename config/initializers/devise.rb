51bb215b567ef3c89bc1b246026bd22a2abcc4de
trox115 <tofernandes67@gmail.com>
2019-10-05 00:17:47 +0100

    run Rubocop

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -296,5 +296,5 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
-  config.omniauth :facebook, "755422288233320", "a2287dfabc963296575a8e2f9ccb86b4", callback_url: "http://localhost:3000/users/auth/facebook/callback"
+  config.omniauth :facebook, '755422288233320', 'a2287dfabc963296575a8e2f9ccb86b4', callback_url: 'http://localhost:3000/users/auth/facebook/callback'
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,2 +1,4 @@
+# frozen_string_literal: true
+
 Rails.application.config.middleware.use OmniAuth::Builder do
 end
