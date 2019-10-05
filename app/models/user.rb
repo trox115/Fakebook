580ba7c280fc58838b91fe3b1c03bcff1ad966d7
Carlos Del Real <carloshdelreal@gmail.com>
2019-10-04 20:31:12 -0500

    Set up environment variables

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,8 +57,8 @@ class User < ApplicationRecord
   # Facebook Omniauth
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
+      if data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
+        user.email = data['email'] if user.email.blank?
       end
     end
   end
@@ -67,7 +67,7 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name   # assuming the user model has a name
+      user.name = auth.info.name # assuming the user model has a name
       # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
       # user.skip_confirmation!
