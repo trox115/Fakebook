ffae4e733bf3fa7e3708acb729ec0a047c84d848
trox115 <tofernandes67@gmail.com>
2019-10-05 01:07:44 +0100

    trying one more thing

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
+config.omniauth :facebook, '755422288233320', 'a2287dfabc963296575a8e2f9ccb86b4', callback_url: 'http://localhost:3000/users/auth/facebook/callback'
 end
