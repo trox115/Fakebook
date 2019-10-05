3d2eb3a142dac6c273c98d3226d81afac3c624d2
trox115 <tofernandes67@gmail.com>
2019-10-05 01:47:24 +0100

    configured ssl certificate

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -296,5 +296,5 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
-config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: 'email', info_fields: 'email, name'
+config.omniauth :facebook, "FACEBOOK_KEY", "FACEBOOK_SECRET", client_options: { ssl: { ca_path: '/usr/lib/ssl/certs/ca-certificates.crt' } }
 end
