d6c41f156dd2b95942174874f18567c2dae4c7ed
trox115 <tofernandes67@gmail.com>
2019-10-05 01:16:14 +0100

    using certificate heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-config.omniauth :facebook, '755422288233320', 'a2287dfabc963296575a8e2f9ccb86b4', callback_url: 'http://localhost:3000/users/auth/facebook/callback'
+
+config.omniauth :facebook, "755422288233320", "a2287dfabc963296575a8e2f9ccb86b4",
+  client_options: { ssl: { ca_path: '/usr/lib/ssl/certs/ca-certificates.crt' } }
 end
