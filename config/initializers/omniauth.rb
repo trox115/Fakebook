930f0480f36da21a23f73058799dbefab71a7ce0
trox115 <tofernandes67@gmail.com>
2019-10-05 01:19:33 +0100

    omniauth facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,4 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
 
-config.omniauth :facebook, "755422288233320", "a2287dfabc963296575a8e2f9ccb86b4",
-  client_options: { ssl: { ca_path: '/usr/lib/ssl/certs/ca-certificates.crt' } }
 end
