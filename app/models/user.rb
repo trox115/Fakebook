8fc398e6951cd526862db3a969d702abebf45f0a
trox115 <tofernandes67@gmail.com>
2019-10-08 15:42:06 +0100

    users can upload profile pic

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
 
   has_many :friends, through: :friendships
   has_many :friendships
-
+  has_one_attached :image
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 
   def friends
